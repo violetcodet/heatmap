d5d1cf76e29647df8a2c9c3d22061f26217f26a2
kwauchope <kelvin@kelvin-laptop-wifi.lan>
2014-08-22 09:56:38 +1000

    added image comaparison tests and split out the repeated saveKML functionality

--- test/tests.py
+++ test/tests.py
@@ -10,113 +10,83 @@ except ImportError:
 import heatmap
 from heatmap import colorschemes
 
+
 class TestHeatmap(unittest.TestCase):
     """unittests for TestHeatmap"""
+
+    def heatmapImage(self,name,pts,kwargs={},saveKML=False):
+        img = self.heatmap.heatmap(pts, **kwargs)
+        self.assertTrue(isinstance(img, Image.Image))
+        if (saveKML):
+          self.heatmap.saveKML(name+".kml")
+        else:
+          img.save(name+".png")
+        return img
     
     def setUp(self):
         self.heatmap = heatmap.Heatmap()
     
     def test_heatmap_random_defaults(self):
         pts = [(random.random(), random.random()) for x in range(400)]
-        img = self.heatmap.heatmap(pts)
-        img.save("01-400-random.png")
-        self.assertTrue(isinstance(img, Image.Image))
+        self.heatmapImage("01-400-random", pts)
     
     def test_heatmap_vert_line(self):
         pts = [(50, x) for x in range(100)]
-        img = self.heatmap.heatmap(pts, area=((0, 0), (200, 200)))
-        img.save("02-vert-line.png")
-        self.assertTrue(isinstance(img, Image.Image))
+        self.heatmapImage("02-vert-line", pts, kwargs={"area" : ((0, 0), (200, 200))})
 
     def test_heatmap_horz_line(self):
         pts = [(x, 300) for x in range(600, 700)]
-        img = self.heatmap.heatmap(pts, size=(800,400), area=((0, 0), (800, 400)))
-        img.save("03-horz-line.png")
-        self.assertTrue(isinstance(img, Image.Image))
+        self.heatmapImage("03-horz-line", pts, kwargs={ "size" : (800,400), "area" : ((0, 0), (800, 400))})
 
     def test_heatmap_random(self):
-        pts = [(random.random(), random.random()) for x in range(40000)]
         # this should also generate a warning on stderr of overly dense
-        img = self.heatmap.heatmap(pts, dotsize=25, opacity=128)
-        img.save("04-40k-random.png")
-        self.assertTrue(isinstance(img, Image.Image))
+        pts = [(random.random(), random.random()) for x in range(40000)]
+        self.heatmapImage("04-40k-random", pts , kwargs={ "dotsize" : 25, "opacity" : 128})
 
     def test_heatmap_square(self):
         pts = [(x*100, 50) for x in range(2, 50)]
         pts.extend([(4850, x*100) for x in range(2, 50)])
         pts.extend([(x*100, 4850) for x in range(2, 50)])
         pts.extend([(50, x*100) for x in range(2, 50)])
-        img = self.heatmap.heatmap(pts, dotsize=100, area=((0,0), (5000, 5000)))
-        img.save("05-square.png")
-        self.assertTrue(isinstance(img, Image.Image))
+        self.heatmapImage("05-square", pts , kwargs={ "dotsize" : 100, "area" : ((0,0), (5000, 5000)) })
 
     def test_heatmap_single_point(self):
         pts = [(random.uniform(-77.012, -77.050), 
                 random.uniform(38.888, 38.910)) for x in range(100)]
-        img = self.heatmap.heatmap(pts)
-        self.heatmap.saveKML("06-wash-dc.kml")
-        self.assertTrue(isinstance(img, Image.Image))
+        self.heatmapImage("06-wash-dc", pts)
 
     def test_heatmap_weighted(self):
         #normal should be the same as 100%, 75% should have same pattern but smaller
         pts = [(random.uniform(30,40), random.uniform(-30,-40)) for x in range(400)]
-        # this should also generate a warning on stderr of overly dense
-        img = self.heatmap.heatmap(pts)
-        img.save("07-400-normal.png")
-        self.heatmap.saveKML("07-400-normal.kml")
-        self.assertTrue(isinstance(img, Image.Image))
-        img = self.heatmap.heatmap(list(map( lambda x_y : (x_y[0],x_y[1],1), pts)), weighted=1)
-        img.save("07-400-100percent.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("07-400-100percent.kml")
-        img = self.heatmap.heatmap(list(map( lambda x_y : (x_y[0],x_y[1],.75), pts)), weighted=1)
-        img.save("07-400-75percent.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("07-400-75percent.kml")
+        norm = self.heatmapImage("07-400-normal", pts, saveKML=True)
+        weight = self.heatmapImage("07-400-100percent", list(map( lambda x_y : (x_y[0],x_y[1],1), pts)), kwargs={ "weighted" : 1 }, saveKML=True)
+        self.assertEqual(norm,weight)
+        weight2 = self.heatmapImage("07-400-75percent", list(map( lambda x_y : (x_y[0],x_y[1],.75), pts)), kwargs={ "weighted" : 1 }, saveKML=True)
+        self.assertNotEqual(norm,weight2)
 
     def test_heatmap_random_datatypes(self):
         #all of the below should turn out to be the same, if not there are issues
         pts = tuple((random.random(),random.random(),1) for x in range(400))
-        img = self.heatmap.heatmap(tuple(map(lambda x_y_z : (x_y_z[0],x_y_z[1]), pts)))
-        img.save("08-400-tupleoftuples.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-tupleoftuples.kml")
-        img = self.heatmap.heatmap(pts, weighted=1)
-        img.save("08-400-tupleoftuplesweighted.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-tupleoftuplesweighted.kml")
-        img = self.heatmap.heatmap(list(map(lambda x_y_z : (x_y_z[0],x_y_z[1]), pts)))
-        img.save("08-400-arrayoftuples.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-arrayoftuples.kml")
-        img = self.heatmap.heatmap(list(pts), weighted=1)
-        img.save("08-400-arrayoftuplesweighted.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-arrayoftuplesweighted.kml")
-        img = self.heatmap.heatmap(tuple(map(lambda x_y_z : [x_y_z[0],x_y_z[1]], pts)))
-        img.save("08-400-tupleofarrays.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-tupleofarrays.kml")
-        img = self.heatmap.heatmap(tuple(map(lambda x_y_z : [x_y_z[0],x_y_z[1],x_y_z[2]], pts)), weighted=1)
-        img.save("08-400-tupleofarraysweighted.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-tupleofarrayweighted.kml")
-        img = self.heatmap.heatmap(list(map(lambda x_y_z : [x_y_z[0],x_y_z[1]], pts)))
-        img.save("08-400-arrayofarrays.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-arrayofarrays.kml")
-        img = self.heatmap.heatmap(list(map(lambda x_y_z : [x_y_z[0],x_y_z[1],x_y_z[2]], pts)), weighted=1)
-        img.save("08-400-arrayofarraysweighted.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-arrayofarraysweighted.kml")
-        img = self.heatmap.heatmap(sum(map(lambda x_y_z : [x_y_z[0],x_y_z[1]], pts),[]))
-        img.save("08-400-flat.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-flat.kml")
-        img = self.heatmap.heatmap(sum(map(lambda x_y_z : [x_y_z[0],x_y_z[1],x_y_z[2]], pts),[]), weighted=1)
-        img.save("08-400-flatweighted.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("08-400-flatweighted.kml")
+        tt = self.heatmapImage("08-400-tupleoftuples", tuple(map(lambda x_y_z : (x_y_z[0],x_y_z[1]), pts)), saveKML=True)
+        ttw = self.heatmapImage("08-400-tupleoftuplesweighted", pts , kwargs = { "weighted" : True }, saveKML=True)
+        at = self.heatmapImage("08-400-arrayoftuples", list(map(lambda x_y_z : (x_y_z[0],x_y_z[1]), pts)), saveKML=True)
+        atw = self.heatmapImage("08-400-arrayoftuplesweighted", list(pts), kwargs = { "weighted" : True }, saveKML=True)
+        ta = self.heatmapImage("08-400-tupleofarrays", tuple(map(lambda x_y_z : [x_y_z[0],x_y_z[1]], pts)), saveKML=True)
+        taw = self.heatmapImage("08-400-tupleofarraysweighted", tuple(map(lambda x_y_z : [x_y_z[0],x_y_z[1],x_y_z[2]], pts)), kwargs = { "weighted" : True }, saveKML=True)
+        aa = self.heatmapImage("08-400-arrayofarrays", list(map(lambda x_y_z : [x_y_z[0],x_y_z[1]], pts)), saveKML=True)
+        aaw = self.heatmapImage("08-400-arrayofarraysweighted", list(map(lambda x_y_z : [x_y_z[0],x_y_z[1],x_y_z[2]], pts)), kwargs = { "weighted" : True }, saveKML=True)
+        f = self.heatmapImage("08-400-flat", sum(map(lambda x_y_z : [x_y_z[0],x_y_z[1]], pts),[]), saveKML=True)
+        fw = self.heatmapImage("08-400-flatweighted", sum(map(lambda x_y_z : [x_y_z[0],x_y_z[1],x_y_z[2]], pts),[]), kwargs = { "weighted" : True }, saveKML=True)
+        self.assertEqual(tt,ttw)
+        self.assertEqual(tt,ttw)
+        self.assertEqual(tt,at)
+        self.assertEqual(tt,atw)
+        self.assertEqual(tt,ta)
+        self.assertEqual(tt,taw)
+        self.assertEqual(tt,aa)
+        self.assertEqual(tt,aaw)
+        self.assertEqual(tt,f)
+        self.assertEqual(tt,fw)
 
     def test_heatmap_area(self):
       MAX_SIZE=8192
@@ -143,52 +113,41 @@ class TestHeatmap(unittest.TestCase):
          PPD = float((width-dotsize/2))/(east-west)
       dotDegrees = dotsize/2/PPD
       bounds = ((west-dotDegrees, south-dotDegrees),(east+dotDegrees,north+dotDegrees))
-      img = self.heatmap.heatmap(pts, size = (width, height), dotsize = dotsize, area=bounds, weighted = 1)
-      img.save("11-400-areaTest.png")
-      self.assertTrue(isinstance(img, Image.Image))
-      self.heatmap.saveKML("11-400-areaTest.kml")
-      img = self.heatmap.heatmap(pts, size = (width, height), dotsize = dotsize, weighted = 1)
-      img.save("11-400-areaTestNormal.png")
-      self.assertTrue(isinstance(img, Image.Image))
-      self.heatmap.saveKML("11-400-areaTestNormal.kml")
+      #these should be the same except less cutin off from manual area
+      self.heatmapImage("11-400-areaTest", pts, { "size" : (width, height), "dotsize" : dotsize, "area" : bounds, "weighted" : 1}, saveKML = True)
+      self.heatmapImage("11-400-areaTestNormal", pts , kwargs = { "size" : (width, height), "dotsize" : dotsize, "weighted" : 1}, saveKML = True)
 
     def test_heatmap_random_proj(self):
-        #4087 should be the same as 'normal'
         pts = [(random.uniform(-180,180),random.uniform(-90,90)) for x in range(400)]
-        img = self.heatmap.heatmap(pts)
-        img.save("09-400-normal.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("09-400-normal.kml")
-        img = self.heatmap.heatmap(pts,srcepsg='EPSG:4326')
-        img.save("09-400-EPSG3857.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("09-400-EPSG3857.kml")
-        img = self.heatmap.heatmap(pts, srcepsg='EPSG:4326',dstepsg='EPSG:4087')
-        img.save("09-400-EPSG4087.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("09-400-EPSG4087.kml")
-        img = self.heatmap.heatmap(pts, srcepsg='EPSG:3857',dstepsg='EPSG:4087')
-        img.save("09-400-EPSG4087.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("09-400-EPSG4087.kml")
+        norm = self.heatmapImage("09-400-normal", pts, saveKML = True)
+        #4087 should be the same as 'normal' as no conversion required, kml boundary should be different (not tested) though as not converted to 4326
+        epsg4087 = self.heatmapImage("90-400-EPSG4087", pts, kwargs = { "srcepsg" : "EPSG:4087", "dstepsg" : "EPSG:4087"}, saveKML = True)
+        self.assertEqual(norm,epsg4087)
+        #4326 should be roughly the same as 'normal' but not the same as WGS84 != 4087
+        epsg4326 = self.heatmapImage("09-400-EPSG4326", pts, kwargs = { "srcepsg" : "EPSG:4326", "dstepsg" : "EPSG:4087" }, saveKML = True)
+        self.assertNotEqual(norm,epsg4326)
+        #3857DST should be well different
+        epsg3857DST = self.heatmapImage("09-400-EPSG3857DST", pts, kwargs = { "srcepsg" : "EPSG:4326"}, saveKML = True)
+        self.assertNotEqual(norm,epsg3857DST)
+        #testing conversion of src epsg, image is possibly similar do to linearity at the equator but KML boundary should be very different (not tested)
+        epsg3857 = self.heatmapImage("09-400-EPSG3857", pts, kwargs = { "srcepsg" : "EPSG:3857", "dstepsg" : "EPSG:4087" }, saveKML = True)
 
     def test_heatmap_weighted_proj(self):
-        #normal should be the same as 4087
-        #3857 and 4087 should roughly meet at 0,0 as symetrical around the equator
         pts = [(x*2,x, 1 if x==0 else 0.75) for x in range(-89,90)]
-        img = self.heatmap.heatmap(pts, size = (2048, 1024), dotsize = 50, weighted = 1)
-        img.save("10-400-normal.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("10-400-normal.kml")
-        img = self.heatmap.heatmap(pts,srcepsg='EPSG:4326', size = (2048, 1024), dotsize = 50, weighted = 1)
-        img.save("10-400-EPSG3857.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("10-400-EPSG3857.kml")
-        img = self.heatmap.heatmap(pts, srcepsg='EPSG:4326',dstepsg='EPSG:4087', size = (2048, 1024), dotsize = 50, weighted = 1)
-        img.save("10-400-EPSG4087.png")
-        self.assertTrue(isinstance(img, Image.Image))
-        self.heatmap.saveKML("10-400-EPSG4087.kml")
-
+        norm = self.heatmapImage("10-400-normal", pts, kwargs = { "size" : (2048, 1024), "dotsize" : 50, "weighted" : 1}, saveKML = True)
+        #4087 should be the same as 'normal' as no conversion required, kml boundary should be different (not tested) though as not converted to 4326
+        epsg4087 = self.heatmapImage("10-400-EPSG4087", pts, kwargs = { "srcepsg" : "EPSG:4087", "dstepsg" : "EPSG:4087", "size" : (2048, 1024), "dotsize" : 50, "weighted" : 1}, saveKML = True)
+        self.assertEqual(norm,epsg4087)
+        #4326 should be roughly the same as 'normal' but not the same as WGS84 != 4087
+        norm = self.heatmapImage("10-400-normal", pts, kwargs = { "size" : (2048, 1024), "dotsize" : 50, "weighted" : 1}, saveKML = True)
+        epsg4326 = self.heatmapImage("10-400-EPSG4326SRC", pts, kwargs = { "srcepsg" : "EPSG:4326", "dstepsg" : "EPSG:4087", "size" : (2048, 1024), "dotsize" : 50, "weighted" : 1}, saveKML = True)
+        self.assertNotEqual(norm,epsg4326)
+        #3857DST and 4087 should roughly meet at 0,0 as symetrical around the equator
+        epsg3857DST = self.heatmapImage("10-400-EPSG3857DST", pts, kwargs = { "srcepsg" : "EPSG:4326", "size" : (2048, 1024), "dotsize" : 50, "weighted" : 1}, saveKML = True)
+        self.assertNotEqual(norm,epsg3857DST)
+        #testing conversion of src epsg, image is possibly similar do to linearity at the equator but KML boundary should be very different (not tested)
+        epsg3857 = self.heatmapImage("10-400-EPSG3857", pts, kwargs = { "srcepsg" : "EPSG:3857", "dstepsg" : "EPSG:4087",  "size" : (2048, 1024), "dotsize" : 50, "weighted" : 1 }, saveKML = True)
+    
     def test_heatmap_exceptions(self):
  
       #test invalid (empty) heatmap, should print error to stdout
