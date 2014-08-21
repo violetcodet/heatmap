dbabd096713eb09f50fa0d7944aace74e9617382
kwauchope <kelvin@kelvin-laptop-wifi.lan>
2014-08-21 17:14:01 +1000

    added back version code

--- heatmap/__init__.py
+++ heatmap/__init__.py
@@ -1,5 +1,10 @@
 import sys
 
+try:
+  __version__ = __import__('pkg_resources').get_distribution(__name__).version
+except Exception as e:
+  __version__ = 'unknown'
+
 if sys.hexversion < 0x02060000:
   raise Exception('Heatmap requires python 2.6 or greater.');
 
